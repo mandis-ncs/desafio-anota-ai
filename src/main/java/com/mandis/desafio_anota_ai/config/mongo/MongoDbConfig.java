6bee1b05cb7fb63aeab44e4066e0d0186783a5bf
mandis-ncs <amnd.castro@gmail.com>
2024-08-05 11:39:20 -0300

    feat: First commit

---
+++ src/main/java/com/mandis/desafio_anota_ai/config/mongo/MongoDbConfig.java
@@ -0,0 +1,23 @@
+package com.mandis.desafio_anota_ai.config.mongo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoDbConfig {
+
+    @Bean
+    public MongoDatabaseFactory monoConfigure(){
+        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/products-catalog");
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(){
+        return new MongoTemplate(monoConfigure());
+    }
+
+
+}
