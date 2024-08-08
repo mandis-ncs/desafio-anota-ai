e16ec1b81d12cb311e4f326b271bc88fe2b9885b
mandis-ncs <amnd.castro@gmail.com>
2024-08-08 16:01:07 -0300

    feat: Create exceptions

---
+++ src/main/java/com/mandis/desafio_anota_ai/domain/category/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,4 @@
+package com.mandis.desafio_anota_ai.domain.category.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException{
+}
---
+++ src/main/java/com/mandis/desafio_anota_ai/domain/product/exceptions/ProductNotFoundException.java
@@ -0,0 +1,4 @@
+package com.mandis.desafio_anota_ai.domain.product.exceptions;
+
+public class ProductNotFoundException extends RuntimeException{
+}
